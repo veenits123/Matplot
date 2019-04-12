a0168725be5e9724a76000c56aa552b5127fda2d
Veenits123 <123sveenit@gmail.com>
2019-04-12 12:42:20 +0530

    daily

--- sourceCode/VEE_NIT.math1.py
+++ sourceCode/VEE_NIT.math1.py
@@ -4,7 +4,7 @@ plt.ylabel('some numbers')
 plt.show()
 
 import matplotlib.pyplot as plt
-plt.plot([1,2,3,4], [1,4,9,16], 'ro')
+plt.plot([1,2,3,4,5], [1,4,9,16,25], 'ro')
 plt.axis([0, 6, 0, 20])
 plt.show()
 
